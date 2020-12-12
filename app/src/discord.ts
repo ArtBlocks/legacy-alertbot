3c7a1bd94f4cd63f8e538ecd8ff0fe1479bbb7ae
Steve Klebanoff <steve@0xproject.com>
2020-12-12 14:37:21 -0800

    fix image url for discord

--- app/src/discord.ts
+++ app/src/discord.ts
@@ -1,14 +1,22 @@
-import { ArtBlockInfo, ArtBlocksResponse } from './artblocks_api';
-import axios from 'axios';
-import {config} from './config';
+import { ArtBlockInfo, ArtBlocksResponse } from "./artblocks_api";
+import axios from "axios";
+import { config } from "./config";
 
-export const discordAlertForArtBlock = async (artBlock: ArtBlockInfo, twitterUrl: string) => {
-    const discordText = `${artBlock.name} minted${artBlock.mintedBy ? ` by ${artBlock.mintedBy}` : ''}. \n\n https://artblocks.io/token/${artBlock.tokenID} \n ${twitterUrl}`;
-    
-    return axios.post(config.discordWebhookUrl, {
-        content: discordText,
-        embeds: [
-            {image: {url:artBlock.image}}
-        ]
-    });
-}
+export const discordAlertForArtBlock = async (
+  artBlock: ArtBlockInfo,
+  twitterUrl: string
+) => {
+  const discordText = `${artBlock.name} minted${
+    artBlock.mintedBy ? ` by ${artBlock.mintedBy}` : ""
+  }. \n\n https://artblocks.io/token/${artBlock.tokenID} \n ${twitterUrl}`;
+
+  const imageUrl = artBlock.image.replace(
+    "api.artblocks.io",
+    "testnetapi.artblocks.io"
+  );
+
+  return axios.post(config.discordWebhookUrl, {
+    content: discordText,
+    embeds: [{ image: { url: imageUrl } }],
+  });
+};
