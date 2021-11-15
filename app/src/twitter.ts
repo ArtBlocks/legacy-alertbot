8197a293c6b3aad6930af74d9412be524d8c6e68
ryley-o <30364988+ryley-o@users.noreply.github.com>
2021-11-15 12:26:10 -0800

    Update to new twitter api and add timeout for robustness

--- app/src/twitter.ts
+++ app/src/twitter.ts
@@ -1,23 +1,40 @@
 import { config } from "./config";
-import { TwitterClient } from "twitter-api-client";
+import { TwitterApi } from "twitter-api-v2";
 import axios from "axios";
 import { ArtBlockInfo, ArtBlocksResponse } from "./artblocks_api";
 import { sleep } from "./utils";
 
 const IMAGE_RETRIES = 35;
 const IMAGE_RETRY_DELAY_MS = 25 * 1000;
+const TWITTER_TIMEOUT_MS = 14 * 1000;
 
-// console.log({config});
-export const twitterClient = new TwitterClient({
-  apiKey: config.twitterApiKey,
-  apiSecret: config.twitterApiSecret,
+export const twitterClientV2 = new TwitterApi({
+  appKey: config.twitterApiKey,
+  appSecret: config.twitterApiSecret,
   accessToken: config.twitterOauthToken,
-  accessTokenSecret: config.twitterOauthSecret,
+  accessSecret: config.twitterOauthSecret,
 });
 
 export interface Response {
   data: any;
 }
+
+function timeout(timeoutMs: number, failureMessage: string): Promise<never> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject(failureMessage), timeoutMs);
+  });
+}
+
+const uploadTwitterMediaWithTimeout = async (
+  timeoutMs: number,
+  based: Buffer
+) => {
+  // use race function to timeout because twitter library doesn't timeout
+  return Promise.race([
+    timeout(TWITTER_TIMEOUT_MS, "Twitter post timed out"),
+    twitterClientV2.v1.uploadMedia(based, { type: "png" }),
+  ]);
+};
 
 const getImageResp = async (imageUrl: string): Promise<Response> => {
   for (let i = 0; i < IMAGE_RETRIES; i++) {
@@ -38,15 +55,18 @@ const getImageResp = async (imageUrl: string): Promise<Response> => {
 export const uploadTwitterImage = async (
   imageUrl: string
 ): Promise<string | undefined> => {
+  console.log("Getting image from artblocks at: ", imageUrl);
   const imageResp = await getImageResp(imageUrl);
   const imageData = imageResp.data as any;
-  const based = Buffer.from(imageData, "binary").toString("base64");
+  const based = Buffer.from(imageData, "binary");
 
   try {
-    const uploadRes = await twitterClient.media.mediaUpload({
-      media_data: based,
-    });
-    return uploadRes.media_id_string;
+    console.log("Uploading received image to Twitter");
+    const uploadRes = await uploadTwitterMediaWithTimeout(
+      TWITTER_TIMEOUT_MS,
+      based
+    );
+    return uploadRes;
   } catch (e) {
     console.error(e);
     return undefined;
@@ -67,14 +87,13 @@ export const tweetArtblock = async (artBlock: ArtBlockInfo) => {
     console.error("no media id returned, not tweeting");
     return;
   }
-
+  console.log(`Uploaded image ${imageUrl} complete. Tweeting...`);
   const tweetText = `${artBlock.name} minted${
     artBlock.mintedBy ? ` by ${artBlock.mintedBy}` : ""
   }. \n\n https://artblocks.io/token/${artBlock.tokenID}`;
   console.log(`Tweeting ${tweetText}`);
 
-  const tweetRes = await twitterClient.tweets.statusesUpdate({
-    status: tweetText,
+  const tweetRes = await twitterClientV2.v1.tweet(tweetText, {
     media_ids: mediaId,
   });
 
