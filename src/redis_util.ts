b0dd9b8e7155088d973dfb57d1cbf39bfcd16058
ryley-o <30364988+ryley-o@users.noreply.github.com>
2021-12-15 15:54:19 -0800

    add Procfile, need dev dependencies for prod on heroku, new redis url env var

--- src/redis_util.ts
+++ src/redis_util.ts
@@ -2,8 +2,7 @@ import * as redis from 'redis';
 import { promisify } from 'util';
 
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST || 'redis',
-    port: 6379,
+    url: process.env.REDIS_URL
 });
 
 const getAsyncRaw = promisify(redisClient.get);
