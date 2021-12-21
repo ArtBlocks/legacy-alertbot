00502461c335ea0ef3373b7d14fc2943aff129e6
Jonathan B <jon@artblocks.io>
2021-12-21 15:01:09 -0500

    adds qeueu processing

---
+++ src/mint_queue.ts
@@ -0,0 +1,31 @@
+import { sendToDiscord } from "./discord";
+import { sendToTwitter } from "./twitter";
+import {
+  getArtblockInfo,
+  getMinterAddress,
+  getOpenseaInfo,
+} from "./api_data";
+import * as Queue from "bull"
+
+export const mintQueue = new Queue('mint alert queue')
+
+mintQueue.process(async (job: {data: {tokenId: string, contractVersion: 'original' | 'v2'}}, done: () => void) => {
+  const {tokenId, contractVersion } = job.data
+  console.log("[INFO] Processing Token #", tokenId)
+  console.log("[INFO] Fetching Complete data for", tokenId);
+  const artBlock = await getArtblockInfo(tokenId);
+  const minterAddress = await getMinterAddress(tokenId, contractVersion);
+  const openseaName = await getOpenseaInfo(minterAddress);
+  let mintedBy;
+  if (openseaName) {
+    mintedBy = openseaName;
+  } else if (minterAddress) {
+    mintedBy = minterAddress;
+  }
+  const artBlockWithOwner = {...artBlock, mintedBy}
+  sendToTwitter(artBlockWithOwner, contractVersion);
+  sendToDiscord(artBlockWithOwner);
+
+  console.log("[INFO] Token Processed #", tokenId )
+  done()
+})
