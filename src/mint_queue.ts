139127fb3127037281bc8b9afc80f43c2f323e23
Jonathan B <jon@artblocks.io>
2021-12-21 16:03:48 -0500

    add concurrency

--- src/mint_queue.ts
+++ src/mint_queue.ts
@@ -1,31 +1,6 @@
-import { sendToDiscord } from "./discord";
-import { sendToTwitter } from "./twitter";
-import {
-  getArtblockInfo,
-  getMinterAddress,
-  getOpenseaInfo,
-} from "./api_data";
+
 import * as Queue from "bull"
 
 export const mintQueue = new Queue('mint alert queue',  process.env.REDIS_URL)
 
-mintQueue.process(async (job: {data: {tokenId: string, contractVersion: 'original' | 'v2'}}, done: () => void) => {
-  const {tokenId, contractVersion } = job.data
-  console.log("[INFO] Processing Token #", tokenId)
-  console.log("[INFO] Fetching Complete data for", tokenId);
-  const artBlock = await getArtblockInfo(tokenId);
-  const minterAddress = await getMinterAddress(tokenId, contractVersion);
-  const openseaName = await getOpenseaInfo(minterAddress);
-  let mintedBy;
-  if (openseaName) {
-    mintedBy = openseaName;
-  } else if (minterAddress) {
-    mintedBy = minterAddress;
-  }
-  const artBlockWithOwner = {...artBlock, mintedBy}
-  sendToTwitter(artBlockWithOwner, contractVersion);
-  sendToDiscord(artBlockWithOwner);
-
-  console.log("[INFO] Token Processed #", tokenId )
-  done()
-})
+mintQueue.process(5, "./processor.ts");
