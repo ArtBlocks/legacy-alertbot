e9d53a3d40992c8e8b95f876d9904a1ee73dd7e8
ryley-o <30364988+ryley-o@users.noreply.github.com>
2021-12-21 16:26:45 -0800

    fix by using non-concurrent queue, add readme w/local dev instructions

--- src/mint_queue.ts
+++ src/mint_queue.ts
@@ -1,5 +1,34 @@
 const Queue = require("bull")
 
+import { sendToDiscord } from "./discord";
+import { sendToTwitter } from "./twitter";
+import {
+  getArtblockInfo,
+  getMinterAddress,
+  getOpenseaInfo,
+} from "./api_data";
+
 export const mintQueue = new Queue('mint alert queue',  process.env.REDIS_URL)
 
-mintQueue.process(process.env.QUEUE_CONCURRENCY, "./src/processor.js");
+// TODO - get concurrent processors working on this queue
+// mintQueue.process(process.env.QUEUE_CONCURRENCY, "./src/processor.js");
+mintQueue.process(async (job: {data: {tokenId: string, contractVersion: 'original' | 'v2'}}, done: () => void) => {
+  const {tokenId, contractVersion } = job.data
+  console.log("[INFO] Processing Token #", tokenId)
+  console.log("[INFO] Fetching Complete data for", tokenId);
+  const artBlock = await getArtblockInfo(tokenId);
+  const minterAddress = await getMinterAddress(tokenId, contractVersion);
+  const openseaName = await getOpenseaInfo(minterAddress);
+  let mintedBy;
+  if (openseaName) {
+    mintedBy = openseaName;
+  } else if (minterAddress) {
+    mintedBy = minterAddress;
+  }
+  const artBlockWithOwner = {...artBlock, mintedBy}
+  sendToTwitter(artBlockWithOwner, contractVersion);
+  sendToDiscord(artBlockWithOwner);
+
+  console.log("[INFO] Token Processed #", tokenId )
+  done()
+})
