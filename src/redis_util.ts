84142a38fb12eb66a3044c3182cfa82a9387c303
Jonathan B <jon@artblocks.io>
2021-12-29 15:10:02 -0500

    add back

--- src/redis_util.ts
+++ src/redis_util.ts
@@ -1,8 +1,9 @@
+import { promisify } from 'util';
 const Redis = require('ioredis');
 var url   = require('url');
 
 const redis_uri = url.parse(process.env.REDIS_URL);
-export const redisClient = new Redis({
+const redisClient = new Redis({
   port: Number(redis_uri.port) + 1,
   host: redis_uri.hostname,
   password: redis_uri.auth.split(':')[1],
@@ -11,4 +12,10 @@ export const redisClient = new Redis({
     rejectUnauthorized: false,
     requestCert: true,
     agent: false
-  }})
+  }
+})
+
+const getAsyncRaw = promisify(redisClient.get);
+export const getRedisAsync = getAsyncRaw.bind(redisClient) as typeof getAsyncRaw;
+const setAsyncRaw = promisify(redisClient.set);
+export const setRedisAsync = setAsyncRaw.bind(redisClient) as typeof setAsyncRaw;
