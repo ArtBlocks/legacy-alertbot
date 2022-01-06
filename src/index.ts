9d814a2f870a8884ed4f0f26e890f6c19700bfa2
Jonathan B <jon@artblocks.io>
2022-01-06 15:45:45 -0500

    fix allowed and only use ab contracts

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,18 @@ const port = process.env.PORT || 8000
 app.use(express.json())
 
 
+const contracts = () => {
+  const addys = {
+    mainnet: ["0x059edd72cd353df5106d2b9cc5ab83a52287ac3a", "0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270"],
+    dev: ["0x87c6e93fc0b149ec59ad595e2e187a4e1d7fdc25"]
+  }
+  if(process.env.NODE_ENV === 'production' ) {
+    return addys.mainnet
+  } else {
+    return addys.dev
+  }
+}
+
 const allowed = (webhookHeader: string) => {
   if(webhookHeader === config.webhookSecret || process.env.NODE_ENV === "test") {
     return true
@@ -17,11 +29,15 @@ const allowed = (webhookHeader: string) => {
   }
 }
 
+const contractAllowed = (contract: string) => {
+  return contracts().includes(contract)
+}
+
 app.post('/', (req: any, res: any) => {
-  if(allowed) {
+  if(allowed(req.get('webhook_secret'))) {
     const newData = req?.body?.event?.data?.new
     const oldData = req?.body?.event?.data?.old
-    if(newData && !oldData.image_id) {
+    if(newData && !oldData.image_id && contractAllowed(newData?.contract_address)) {
       console.log("[INFO] Received Webhook for ", newData)
       const tokenId = newData?.token_id
       const ownerAddress = newData?.owner_address
